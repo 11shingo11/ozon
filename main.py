25cbfb34c9412caa2781e8d2d26924e0e4bbdc9e
11shingo11 <shin2312@icloud.com>
2023-03-06 13:39:21 +0300

    update

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from random import randrange
+
+import openpyxl as openpyxl
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium import webdriver
@@ -25,80 +27,97 @@ driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
         delete window.cdc_adoQpoasnfa76pfcZLmcfl_Symbol;
   '''
 })
-base_url="https://www.ozon.ru/category/zaryadnye-ustroystva-i-dok-stantsii-15920/"
-url = "https://www.ozon.ru/category/zaryadnye-ustroystva-i-dok-stantsii-15920/"
+url = input("please input your url: ")
+rait_filter = float(input("input filter for product raiting: "))
+comment_filter = int(input("input filter for num comments: "))
+if url.endswith('/'):
+    base_url = f"{url}?"
+else:
+    base_url = f"{url}&"
+
+
+
 i = 1
-Base = declarative_base()
-
-class Charger(Base):
-    __tablename__ = 'chargers'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    comments = Column(String)
-    price = Column(Integer)
-    url = Column(String)
-    raiting = Column(String)
-
-engine = create_engine('postgresql://postgres:2312@localhost:5432/ozon')
-# metadata = MetaData()
-#
-# my_table = Table('chargers', metadata, autoload_with=engine)
-# with engine.connect() as conn:
-#     conn.execute(my_table.delete())
-Session = sessionmaker(bind=engine)
-session = Session()
-idnum = 0
-while i < 279:
+def conn(url, row, idnum, name_class):
     driver.get(url)
     time.sleep(randrange(2, 3))
     driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.PAGE_DOWN)
-    time.sleep(1)
+    time.sleep(randrange(1, 3))
+    # print('found ', len(driver.find_elements(By.CSS_SELECTOR, f"div[class='{name_class}']")), ' elements')
+    row, idnum = parse(name_class=name_class, row=row, idnum=idnum)
+    return row, idnum
+
+
     # в зависимости от карточки товара название этого класса(class='y4j j5y') может меняться параметры внутри этого
     # элемента при проверкке на другой категории полностью совпали.
-    for el in driver.find_elements(By.CSS_SELECTOR, "div[class='y6j jy7']"):
+def parse(name_class, row, idnum, i=1):
+    for el in driver.find_elements(By.CSS_SELECTOR, f"div[class='{name_class}']"):
+        i += 1
         reviews = 0
         raiting = 0
         try:
-            items = []
             for item in el.find_elements(By.CSS_SELECTOR, "span[class='e4f']"):
-                items.append(item)
-            for item in items:
-                if item == items[0]:
-                    raiting = float(item.text)
-                    # print(raiting)
-                if item == items[1]:
+                try:
                     reviews = int(item.text.split(' ')[1])
                     # print(reviews)
-            # тут происходит отбор товаров по кол-ву отзывов, а далее по оценке, только после прохождения отбора по двум
-            # критериям товар занесется в бд
-            if reviews > 1500:
-                if raiting >= 4.8:
-                     idnum += 1
-                     product_url = el.find_element(By.CSS_SELECTOR, "a[class='tile-hover-target j4v v4j']").get_attribute("href")
-                     product_name = el.find_element(By.CSS_SELECTOR, "a[class='tile-hover-target j4v v4j']").text
-                     # print(product_name)
-                     product_price = el.find_element(By.CSS_SELECTOR, "div[class='aa2-a0']").text.split("₽")[0]
-                     charger = Charger(
-                        id=idnum,
-                        name=product_name,
-                        comments=reviews,
-                        price=product_price,
-                        url=product_url,
-                        raiting=raiting
-                     )
-                     session.add(charger)
-                else:
-                    # logging.error(Exception)
-                    pass
-            else:
-                # logging.error(Exception)
-                pass
-        except Exception as e:
-            pass
+                except:
+                    try:
+                        raiting = float(item.text)
+                        # print(raiting)
+                    except:
+                        pass
+            if reviews > comment_filter:
+                if raiting >= rait_filter:
+                    idnum += 1
+                    product_url = el.find_element(By.CSS_SELECTOR, "a[class='tile-hover-target j4v v4j']").get_attribute("href")
+                    product_name = el.find_element(By.CSS_SELECTOR, "a[class='tile-hover-target j4v v4j']").text
+                    # print(product_name)
+                    product_price = el.find_element(By.CSS_SELECTOR, "div[class='aa2-a0']").text.split("₽")[0]
+                    worksheet['A' + str(row)] = idnum
+                    worksheet['B' + str(row)] = raiting
+                    worksheet['C' + str(row)] = product_name
+                    worksheet['D' + str(row)] = product_price
+                    worksheet['E' + str(row)] = reviews
+                    worksheet['F' + str(row)] = product_url
+                    row += 1
+                    # print("add one")
+                    workbook.save('test.xlsx')
+        except:
+            print("not add")
+            continue
 
-    i += 1
-    print("link number:",i)
-    next_url = f"{base_url}?page={i}"
+    return row, idnum
+
+
+h = 1
+workbook = openpyxl.Workbook()
+worksheet = workbook.active
+worksheet['A1'] = 'id'
+worksheet['B1'] = 'raiting'
+worksheet['C1'] = 'product_name'
+worksheet['D1'] = 'product_price'
+worksheet['E1'] = 'rewies'
+worksheet['F1'] = 'product_url'
+row = 2
+idnum = 0
+driver.get(url)
+try:
+    products_num = int(driver.find_element(By.CSS_SELECTOR, "div[class='oe']").text.split(' ')[0])
+except:
+    products_num = int(driver.find_element(By.CSS_SELECTOR, "div[class='faa2']").text.split(' ')[4])
+driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.PAGE_DOWN)
+time.sleep(2)
+name_class = str(driver.find_element(By.CSS_SELECTOR, "div[class='kl2']").get_attribute('innerHTML').split('"')[1])
+products_on_page = (int(len(driver.find_elements(By.CSS_SELECTOR, f"div[class='{name_class}']"))))
+pages = int(products_num/products_on_page)+2
+
+while h != pages:
+    print(url)
+    row, idnum = conn(url, row=row, idnum=idnum, name_class=name_class)
+    h += 1
+    next_url = f"{base_url}page={h}"
     url = next_url
-session.commit()
+    # print(row, idnum)
 
+driver.close()
+driver.quit()
